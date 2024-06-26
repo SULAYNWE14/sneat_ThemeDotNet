5ea81b16a305edd5cd4a2296a5e9e7e29c0441e7
SULAYNWE14 <sulaynwe1411@gmail.com>
2024-06-26 23:36:00 +0900

    Update Theme

---
+++ sneat_ThemeDotNet/Controllers/AccountSettingController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace sneat_ThemeDotNet.Controllers
+{
+    public class AccountSettingController : Controller
+    {
+        public IActionResult Account()
+        {
+            return View();
+        }
+
+        public IActionResult Notification()
+        {
+            return View();
+        }
+
+        public IActionResult Connection()
+        {
+            return View();
+        }
+    }
+}
