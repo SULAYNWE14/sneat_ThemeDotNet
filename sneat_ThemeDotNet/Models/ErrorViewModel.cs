712f2bedfa73896a43ae2a002ecb3fd2f57f7c68
SULAYNWE14 <sulaynwe1411@gmail.com>
2024-06-26 22:29:01 +0900

    add Theme

---
+++ sneat_ThemeDotNet/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace sneat_ThemeDotNet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
